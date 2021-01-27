1be8020ceec2408d6537e40c43f90bde2bfd91c6
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2021-01-27 12:03:49 -0300

    :sparkles:  Adicionando a gravação de grupos de vídeo conferências com o Media Recorder.

---
+++ aula-03/public/pages/room/src/app.js
@@ -0,0 +1,35 @@
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get('room');
+  console.log('this is the room', room);
+
+  const socketUrl = 'http://localhost:3000';
+  const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/'
+    }
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
+  const view = new View();
+  // view.renderVideo({ userId: 'test01', url: 'https://media.giphy.com/media/Cf0a28gTtbQp9K3Gzt/giphy.mp4' });
+  // view.renderVideo({ userId: 'test01', isCurrentId: true, url: 'https://media.giphy.com/media/Cf0a28gTtbQp9K3Gzt/giphy.mp4' });
+  // view.renderVideo({ userId: 'test02', url: 'https://media.giphy.com/media/Cf0a28gTtbQp9K3Gzt/giphy.mp4' });
+  const media = new Media();
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder,
+    peerBuilder
+  };
+
+  Business.initialize(deps);
+}
+
+window.onload = onload
--- restore-packages.sh
+++ restore-packages.sh
@@ -1,3 +1,7 @@
+rm -rf **/**/node_modules;
+cp -r aula-02 aula-03;
+cd aula-03;
+
 for item in `ls`;
 
 do
